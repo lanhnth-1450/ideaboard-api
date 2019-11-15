a4706a43c4df265cd3bd9f88024308b1db97cd8c
Nguyen Thi Huyen Lanh <nguyen.thi.huyen.lanh@sun-asterisk.com>
2019-11-15 09:13:13 +0700

    hot fix: reverse between api port and frontend port

--- config/application.rb
+++ config/application.rb
@@ -34,7 +34,7 @@ module IdeaboardApi
 
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins 'http://localhost:3000'
+        origins 'http://localhost:3001'
         resource '*', :headers => :any, :methods => [:get, :post, :put, :delete, :options]
       end
     end
